7c7f16049604afa20475f662c32a11f40e88aa25
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-04-20 10:03:02 -0300

    Creamos el Proyecto con la Conexion a FireBase

---
+++ app/src/main/java/com/digrutt/preseptorgomara/MainActivity.java
@@ -0,0 +1,21 @@
+package com.digrutt.preseptorgomara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*Este preseptor Gomara va a
+    *
+    * Agregar , Modificar , Leer y Eliminar Alumnos de Determinados Cursos
+    * Administrar las inasistencias que tiene cada Alumno
+    *
+    * */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
