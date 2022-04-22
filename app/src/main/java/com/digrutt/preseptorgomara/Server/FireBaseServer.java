dc0b94144016743c1c13f02d2caa445cf7367037
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-04-22 16:18:45 -0300

    Agregando Funciones Read,Create,Update con FireBase

--- app/src/main/java/com/digrutt/preseptorgomara/Server/FireBaseServer.java
+++ app/src/main/java/com/digrutt/preseptorgomara/Server/FireBaseServer.java
@@ -1,4 +1,146 @@
 package com.digrutt.preseptorgomara.Server;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*Funciones de esta clase
+ *   DE CUALQUIER CURSO O AÑO PODEMOS: CRUD
+ *       +Crear un nuevo Alumno
+ *       +Obtener Todos los Alumnos
+ *       +Actualizar el Nombre del alumno
+ *       +Actualizar inasistencia del alumno
+ *       +
+ * */
+
 public class FireBaseServer {
+
+    private static final String TAG = "FireBaseServer";
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void ObtenerBaseDeDatos(String curso){
+        db.collection("Cursos/" + curso +"/Alumnos")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot document : task.getResult()){
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        }else Log.w(TAG,"Error getting documents." , task.getException());
+
+                    }
+                });
+    }
+
+    public void ActualizarNombre(String id,String nombre,String curso){
+        DocumentReference reference = db.collection("Cursos/" + curso +"/Alumnos").document(id);
+        reference.update("Nombre",nombre).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d(TAG, "DocumentSnapshot successfully updated!");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error updating document", e);
+            }
+        });
+    }
+
+    public void ActualizarInasistencias(String id,float Inasistencia,String curso){
+        DocumentReference reference = db.collection("Cursos/" + curso + "/Alumnos").document(id);
+        reference.update("Inasistencias", Inasistencia).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d(TAG, "DocumentSnapshot successfully updated!");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error updating document", e);
+            }
+        });
+    }
+
+    public void CreateDocumentAlumno(String DNI,String nombre,float inasistenicas,String curso){
+        DocumentReference reference = db.collection("Cursos/" + curso + "/Alumnos").document(DNI);
+        Map newAlumno = new HashMap();
+        newAlumno.put("Nombre",nombre);
+        newAlumno.put("Inasistencias",inasistenicas);
+        reference.set(newAlumno)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG,"Creado Correctamente");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG,"Error al crear el alumno",e);
+            }
+        });
+    }
+
+    /*
+    public String obtenerNombre(String id,String curso){
+        DocumentReference reference = db.collection("Cursos/" + curso + "/Alumnos").document(id);
+        String[] nombre = new String[1];
+        reference
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot documentSnapshot = task.getResult();
+                            if(documentSnapshot.exists()){
+                                String result = String.valueOf(documentSnapshot.getData().get("Nombre"));
+                                nombre[0] = result;
+                            }
+                        }else Log.d(TAG, "get failed with ", task.getException());
+
+                    }
+                });
+        return nombre[0];
+    }*/
+
+    //FALTARIA OBTENER: solamente la inasistencias y el nombre
+    /*
+    public float obtenerInasistencias(String id){
+        DocumentReference reference = db.collection("Cursos/" + curso + "/Alumnos").document(id);
+        float[] nombre = new Float[1];
+        reference
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot documentSnapshot = task.getResult();
+                            if(documentSnapshot.exists()){
+                                String result = String.valueOf(documentSnapshot.getData().get("Inasistencias"));
+                                nombre[0] = result;
+                            }
+                        }else Log.d(TAG, "get failed with ", task.getException());
+
+                    }
+                });
+        return nombre[0];
+    }
+    */
+
+
 }
